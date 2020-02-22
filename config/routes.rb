f2266e1881835e2cfc3f3787b69ad747928337eb
18nguyenl <18nguyenl@gmail.com>
2020-02-21 19:20:07 -0600

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get 'recipe/step2_screen'
   get 'recipe/step3_screen'
   get 'recipe/help_screen'
+  get 'recipe/hamburger_screen'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
